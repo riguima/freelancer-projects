8d49ccdf84eb1a681ce524537a5a96d7376285ab
riguima <richard.alexsander.guima@gmail.com>
2024-01-16 17:50:45 -0300

    fix: now always addind projects

--- freelancer_projects/views.py
+++ freelancer_projects/views.py
@@ -1,41 +1,22 @@
-from flask import render_template
+from flask import render_template, request
 from sqlalchemy import select
 
-from freelancer_projects.api import (get_nine_nine_freelas_projects,
-                                     get_workana_projects)
+from freelancer_projects.config import get_config
 from freelancer_projects.database import Session
 from freelancer_projects.models import Project
-from freelancer_projects.config import get_config
 
 
 def init_app(app):
     @app.get('/')
     def index():
-        projects = get_nine_nine_freelas_projects()
-        projects.extend(get_workana_projects())
-        already_added_projects = []
         with Session() as session:
-            for project in projects:
-                query = select(Project).where(Project.url == project['url'])
-                project_model = session.scalars(query).first()
-                if project_model:
-                    already_added_projects.append(project)
-                else:
-                    project_model = Project(
-                        title=project['title'],
-                        url=project['url'],
-                        project_datetime=project['datetime'],
-                    )
-                    session.add(project_model)
-                    session.commit()
-            for project in already_added_projects:
-                projects.remove(project)
-            for project in session.scalars(select(Project).order_by(Project.project_datetime.desc())).all()[get_config()['MAX_PROJECTS']:]:
-                session.delete(project)
-                session.commit()
+            projects = session.scalars(
+                select(Project).order_by(Project.project_datetime.desc())
+            ).all()
+            page = int(request.args.get('page', 1))
             return render_template(
                 'index.html',
-                projects=session.scalars(
-                    select(Project).order_by(Project.project_datetime.desc())
-                ),
+                projects=projects,
+                page=page,
+                last_page=len(projects) // get_config()['PAGINATION'],
             )
