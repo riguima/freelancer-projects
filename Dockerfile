b9c0eca8fa54c02d3c30f6f9769c70f51dbc0d2d
riguima <richard.alexsander.guima@gmail.com>
2024-01-16 18:32:22 -0300

    now works with one file

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ WORKDIR /app
 COPY . .
 RUN pip install -r requirements.txt
 EXPOSE 5000
-CMD [ "flask", "run", "--host", "0.0.0.0" ]
+ENTRYPOINT [ "flask", "run", "--host", "0.0.0.0" ]
--- app.py
+++ app.py
@@ -1,10 +1,44 @@
+from threading import Thread
+from time import sleep
+
 from flask import Flask
+from sqlalchemy import select
 
 from freelancer_projects import views
+from freelancer_projects.api import (get_nine_nine_freelas_projects,
+                                     get_workana_projects)
+from freelancer_projects.config import get_config
+from freelancer_projects.database import Session
+from freelancer_projects.models import Project
+
+
+def add_projects():
+    while True:
+        projects = get_nine_nine_freelas_projects()
+        projects.extend(get_workana_projects())
+        with Session() as session:
+            for project in projects:
+                query = select(Project).where(Project.url == project['url'])
+                project_model = session.scalars(query).first()
+                if project_model is None:
+                    project_model = Project(
+                        title=project['title'],
+                        url=project['url'],
+                        project_datetime=project['datetime'],
+                    )
+                    session.add(project_model)
+                    session.commit()
+            for project in session.scalars(
+                select(Project).order_by(Project.project_datetime.desc())
+            ).all()[get_config()['MAX_PROJECTS'] :]:
+                session.delete(project)
+                session.commit()
+        sleep(60)
 
 
 def create_app():
     app = Flask(__name__, template_folder='templates', static_folder='static')
+    Thread(target=add_projects).start()
     views.init_app(app)
     return app
 
