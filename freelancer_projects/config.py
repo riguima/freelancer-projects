c25721ee3dc58d6f67deb44c4b01216c4651b8f1
riguima <richard.alexsander.guima@gmail.com>
2024-01-17 10:29:51 -0300

    fix config

--- freelancer_projects/config.py
+++ freelancer_projects/config.py
@@ -1,6 +1,8 @@
+from pathlib import Path
+
 import toml
 
 
 def get_config():
-    config = toml.load(open('.config.toml'))
+    config = toml.load(open(Path('.config.toml').absolute()))
     return config['testing'] if config['TESTING'] else config['default']
