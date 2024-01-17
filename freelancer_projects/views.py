4cbc26bc2a0d3d1da619663a07f31c4fa1c54c0f
riguima <richard.alexsander.guima@gmail.com>
2024-01-17 11:02:03 -0300

    fix pagination

--- freelancer_projects/views.py
+++ freelancer_projects/views.py
@@ -1,5 +1,6 @@
-from flask import render_template, request
+from flask import render_template, request, abort
 from sqlalchemy import select
+from math import ceil
 
 from freelancer_projects.config import get_config
 from freelancer_projects.database import Session
@@ -14,9 +15,14 @@ def init_app(app):
                 select(Project).order_by(Project.project_datetime.desc())
             ).all()
             page = int(request.args.get('page', 1))
+            pagination = get_config()['PAGINATION']
+            last_page = ceil(len(projects) / pagination)
+            if page > last_page:
+                return abort(404)
+            projects = projects[pagination * (page - 1):pagination * (page - 1) + pagination]
             return render_template(
                 'index.html',
                 projects=projects,
                 page=page,
-                last_page=len(projects) // get_config()['PAGINATION'],
+                last_page=last_page,
             )
